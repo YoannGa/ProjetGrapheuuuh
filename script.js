7a006e1f69140543c0d3dc1885bd4c2933d7381a
YoannGa <yoann.gath@gmail.com>
2017-12-21 16:25:06 +0100

    On a pas trop touché;;.

--- script.js
+++ script.js
@@ -1,68 +1,59 @@
 Plotly.d3.csv('https://raw.githubusercontent.com/YoannGa/ProjetGrapheuuuh/master/toulouse.csv', function(err, rows){
-			  <!-- GET https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={90313b881f526a2b3fe7a265084db8ca6c33160c} !-->
+	<!-- GET https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={90313b881f526a2b3fe7a265084db8ca6c33160c} !-->
+	//'https://raw.githubusercontent.com/YoannGa/ProjetGrapheuuuh/master/toulouse.csv'
 			  
-			  var classArray = unpack(rows, 'class');
-			  var classes = [...new Set(classArray)];
+	var classArray = unpack(rows, 'class');
+	var classes = [...new Set(classArray)];
 
-			  function unpack(rows, key) {
-			    return rows.map(function(row) { return row[key]; });
-			  }
+	function unpack(rows, key) {
+		return rows.map(function(row) { return row[key]; });
+	}
 
-			  var data = classes.map(function(classes) {
-			    var rowsFiltered = rows.filter(function(row) {
-			        return (row.class === classes);
-			    });
-			    return {
-			       type: 'scattermapbox',
-			       name: "Stations Vélô",
-			       lat: unpack(rowsFiltered, 'Latitude'),
-			       lon: unpack(rowsFiltered, 'Longitude')
-			    };
-			  });
+	var data = classes.map(function(classes) {
+		var rowsFiltered = rows.filter(function(row) {
+			return (row.class === classes);
+		});
+		return {
+			type: 'scattermapbox',
+			name: "Stations Vélô",
+			lat: unpack(rowsFiltered, 'Latitude'),
+			lon: unpack(rowsFiltered, 'Longitude')
+		};
+	});
 			  
-			  var layout = {
-				
-			     title: 'Stations Velô Toulouse',
-			     font: {
-			         color: 'white'
-			     },
-			    dragmode: 'zoom', 
-			    mapbox: {
-				
-			      center: {
-			        lat: 43.5992512, 
-			        lon: 1.4472688
-			      }, 
-			      domain: {
-			        x: [0, 1], 
-			        y: [0, 1]
-			      }, 
-			      style: 'dark', 
-			      zoom: 11
-			    }, 
-			    margin: {
-			      r: 20, 
-			      t: 40, 
-			      b: 20, 
-			      l: 20, 
-			      pad: 0
-			    }, 
-			    paper_bgcolor: '#191A1A', 
-			    plot_bgcolor: '#191A1A', 
-			    showlegend: true,
-			    annotations: [{
-			       x: 0,
-			       y: 0,
-			       xref: 'paper',
-			       yref: 'paper',
-			         text: 'Source: <a href="https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh" style="color: rgb(255,255,255)">NASA</a>',
-			         showarrow: false
-			     }]
-			  };
+	var layout = {
+		title: 'Stations Velô Toulouse',
+		font: {
+			color: 'white'
+		},
+		dragmode: 'zoom', 
+		mapbox: {
+			center: {
+				lat: 43.5992512, 
+				lon: 1.4472688
+			}, 
+			domain: {
+				x: [0, 1], 
+				y: [0, 1]
+			}, 
+				style: 'dark', 
+				zoom: 11
+		}, 
+		margin: {
+			r: 20, 
+			t: 40, 
+			b: 20, 
+			l: 20, 
+			pad: 0
+		}, 
+		paper_bgcolor: '#191A1A', 
+		plot_bgcolor: '#191A1A', 
+		showlegend: true
+	};
 			  
-			  Plotly.setPlotConfig({
-			    mapboxAccessToken: 'pk.eyJ1IjoiZXRwaW5hcmQiLCJhIjoiY2luMHIzdHE0MGFxNXVubTRxczZ2YmUxaCJ9.hwWZful0U2CQxit4ItNsiQ'
-			  });
+	Plotly.setPlotConfig({
+		mapboxAccessToken: 'pk.eyJ1IjoiZXRwaW5hcmQiLCJhIjoiY2luMHIzdHE0MGFxNXVubTRxczZ2YmUxaCJ9.hwWZful0U2CQxit4ItNsiQ'
+	});
 			  
-			  Plotly.plot('graphDiv', data, layout);
-			});
+	Plotly.plot('graphDiv', data, layout);
+});
